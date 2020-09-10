9c8ef652770eef4f4a34c4a7e535e55351afcbd7
Idan Shalem <idanshalem25@gmail.com>
2020-09-10 10:29:43 +0300

    fixed default route

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,18 +4,22 @@ const urllib = require('urllib')
 
 const City = require('../model/City')
 
-const apiURL = 'http://api.openweathermap.org/data/2.5/weather?q='
-const metricUnits = 'units=metric'
-const apiKey = 'appid=516eda5415931599ef12e92f4733ebf9'
+const apiURL = 'http://api.openweathermap.org/data/2.5/weather?q=' //API url for request a weather by city
+const metricUnits = 'units=metric' //a query param for results in celsius
+const apiKey = 'appid=516eda5415931599ef12e92f4733ebf9' //the API key for getting JSON files
 
 
 router.get('/defaultCity', function(req, res) {
-    urllib.request(`${units=metric}tel-aviv&${metricUnits}&${apiKey}`, function(err, weather) {
+    urllib.request(`tel-aviv&${metricUnits}&${apiKey}`, function(err, weather) {
+        if(err){
+            res.send(err)
+        }
         const defaultCity = new City({
             name: weather.name,
             temperature: weather.main.temp,
             condition: weather.weather.description,
             conditionPic: weather.weather.id
         })
+        defaultCity.save().then(c => res.send(c))
     })
 })
