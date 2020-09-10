3fcbee1df0097f6522f0fd499f02d3bcae785281
Idan Shalem <idanshalem25@gmail.com>
2020-09-10 10:24:40 +0300

    added a default route to tel aviv city

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const urllib = require('urllib')
 
 const City = require('../model/City')
 
-const apiKey = '516eda5415931599ef12e92f4733ebf9'
+const apiURL = 'http://api.openweathermap.org/data/2.5/weather?q='
+const metricUnits = 'units=metric'
+const apiKey = 'appid=516eda5415931599ef12e92f4733ebf9'
+
+
+router.get('/defaultCity', function(req, res) {
+    urllib.request(`${units=metric}tel-aviv&${metricUnits}&${apiKey}`, function(err, weather) {
+        const defaultCity = new City({
+            name: weather.name,
+            temperature: weather.main.temp,
+            condition: weather.weather.description,
+            conditionPic: weather.weather.id
+        })
+    })
+})
