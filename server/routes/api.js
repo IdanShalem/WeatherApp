0f11f49b1ad18906c66fd1931a6eba67d2748145
Idan Shalem <idanshalem25@gmail.com>
2020-09-10 11:57:33 +0300

    added CRUD routes to the api

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,45 +1,65 @@
 const express = require('express')
 const router = express.Router()
-const urllib = require('urllib')
+const axios = require('axios')
 
 const City = require('../model/City')
 
-const apiURL = 'http://api.openweathermap.org/data/2.5/weather?q=' //API url for request a weather by city
-const metricUnits = 'units=metric' //a query param for results in celsius
 const apiKey = 'appid=516eda5415931599ef12e92f4733ebf9' //the API key for getting JSON files
+const getAPI = (cityName) => { 
+    return `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&${apiKey}` }
 
 
-router.get('/defaultCity', function(req, res) {
-    urllib.request(`${apiURL}tel-aviv&${metricUnits}&${apiKey}`, function(err, cityResult) {
-        if(err){
-            res.send(err)
+router.get('/defaultCity', async function(req, res) {
+    try{
+        const weatherData = await axios.get(getAPI('tel-aviv'))
+        const city = {
+            name: weatherData.data.name,
+            temperature: weatherData.data.main.temp,
+            condition: weatherData.data['weather'][0].description,
+            conditionPic: weatherData.data['weather'][0].id
         }
-        let weather = JSON.parse(cityResult.toString())
-        const defaultCity = new City({
-            name: weather.name,
-            temperature: weather.main.temp,
-            condition: weather['weather'][0].description,
-            conditionPic: weather['weather'][0].id
-        })
-        res.send(defaultCity)
-    })
-})
-
-router.get('/city/:cityName', function(req, res) {
-    const { cityName } = req.params
-    urllib.request(`${apiURL}${cityName}&${metricUnits}&${apiKey}`, function(err, cityResult) {
-        if(err){
-            res.send(err)
-        }
-        let weather = JSON.parse(cityResult.toString())
-        const city = new City({
-            name: weather.name,
-            temperature: weather.main.temp,
-            condition: weather['weather'][0].description,
-            conditionPic: weather['weather'][0].id
-        })
         res.send(city)
-    })
+    }
+    catch(error){
+        res.send(error)
+    }
 })
 
+router.get('/city/:cityName', async function(req, res) {
+    const { cityName } = req.params
+    try{
+        const weatherData = await axios.get(getAPI(cityName))
+        const city = {
+            name: weatherData.data.name,
+            temperature: weatherData.data.main.temp,
+            condition: weatherData.data['weather'][0].description,
+            conditionPic: weatherData.data['weather'][0].id
+        }
+        res.send(city)
+    } catch(error){
+        res.send(error)
+    }
+})
+
+router.get('/cities', function(req, res) {
+    City.find({}).then(cities => res.send(cities))
+})
+
+router.post('/city' , function(req, res) {
+    const city = req.body
+    const newCity = new City({...city})
+    newCity.save().then(c => res.send(c))
+})
+
+router.delete('/city/:cityName', function(req, res) {
+    const { cityName } = req.params
+    City.findOneAndDelete({'name': cityName}, function(err, c) {
+        if (err){ 
+            res.send(err) 
+        } 
+        else{ 
+            res.send(c) 
+        }
+    })
+})
 module.exports = router
