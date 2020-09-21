60c2193bbc79594a474b7c7d3b5c2557adf4689b
Idan Shalem <idanshalem25@gmail.com>
2020-09-21 18:38:43 +0300

    connecting to heroku

--- server/model/City.js
+++ server/model/City.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose")
-mongoose.connect('mongodb://localhost/WeatherDB', {useNewUrlParser: true})
 mongoose.set('useFindAndModify', false)
 
 const Schema = mongoose.Schema
